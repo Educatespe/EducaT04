cffcf971397169b6eac538915e65dc3bf1464ac2
Educatespe <educatespe@gmail.com>
2017-06-27 17:57:45 -0500

    Eliminación de NamedQueries y documentación de clases

--- EducaT04-ejb/src/main/java/ec/edu/espe/isi/educat04/model/CapacitacionAlumno.java
+++ EducaT04-ejb/src/main/java/ec/edu/espe/isi/educat04/model/CapacitacionAlumno.java
@@ -13,8 +13,6 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -25,8 +23,7 @@ import javax.validation.constraints.Size;
  */
 @Entity
 @Table(name = "capacitacion_alumno", catalog = "educat04", schema = "")
-@NamedQueries({
-    @NamedQuery(name = "CapacitacionAlumno.findAll", query = "SELECT c FROM CapacitacionAlumno c")})
+
 public class CapacitacionAlumno implements Serializable {
 
     private static final long serialVersionUID = 1L;

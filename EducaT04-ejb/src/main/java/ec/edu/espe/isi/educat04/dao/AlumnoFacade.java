8b7d07dbe946b20694bfe74b11b4440f898db431
Educatespe <educatespe@gmail.com>
2017-06-28 19:53:00 -0500

    Creación de los facade de las clases Alumno y CapacitacionAlumno

---
+++ EducaT04-ejb/src/main/java/ec/edu/espe/isi/educat04/dao/AlumnoFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.isi.educat04.dao;
+
+import ec.edu.espe.isi.educat04.model.Alumno;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author pablo
+ */
+@Stateless
+public class AlumnoFacade extends AbstractFacade<Alumno> {
+
+    @PersistenceContext(unitName = "ec.edu.espe.isi.educat04_EducaT04-ejb_ejb_1PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AlumnoFacade() {
+        super(Alumno.class);
+    }
+    
+}

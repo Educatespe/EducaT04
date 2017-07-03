2a926939779a3b9532912e063bd708bbfafeb01e
Educatespe <educatespe@gmail.com>
2017-07-03 17:41:49 -0500

    Generación de documentación de la cabecera en las clases CapacitacionFacade, CursoFacade, DocenteFacade.

--- EducaT04-ejb/src/main/java/ec/edu/espe/isi/educat04/dao/DocenteFacade.java
+++ EducaT04-ejb/src/main/java/ec/edu/espe/isi/educat04/dao/DocenteFacade.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * MIT Licence
+ * All Right Reserved
  */
 package ec.edu.espe.isi.educat04.dao;
 
